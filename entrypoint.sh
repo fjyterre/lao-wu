f7d675f586809ec8110dedf89b78a49a80cda3ae
fjyterre <usgihi@proton.me>
2024-08-12 07:26:56 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="aab4120e-50f1-495e-bc3c-4abd008cb5b7"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
